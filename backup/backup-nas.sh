9ef9f1f2a674a695b6dfc45de5ebff062509df1b
Rick Brown <ricksbrown73@gmail.com>
2022-07-02 14:10:21 +1000

    Add some more doco to the backup script

--- backup/backup-nas.sh
+++ backup/backup-nas.sh
@@ -7,6 +7,13 @@
 # s3cmd --configure
 # Configure requires you to sign in to the Amazon console and generate Access and Secret Keys
 # https://s3tools.org/s3cmd-howto
+#
+# This creates a .s3cmd config file in the user home directory. If you configure it as one user and then
+# rubn it as another, e.g. root, you need to symlink it, for example, created as user `htpc` but run as root:
+# ln -s /home/htpc/.s3cfg /root/
+#
+# Also I don't think flock likes sharing users, so for testing don't use flock OR use flock only with the user
+# you will using to run from cron.
 
 # "/mnt/rdocs/" "/mnt/jdocs/" "/mnt/vdocs/"
 # /home/htpc/.config/homeassistant/
