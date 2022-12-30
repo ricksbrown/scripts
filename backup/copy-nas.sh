7547dd0d69e60efa2f3e1c9bac4be95aba26e201
Rick Brown <ricksbrown73@gmail.com>
2022-12-30 12:55:36 +1100

    Add NAS copy
    Thinking of eventually retiring it

---
+++ backup/copy-nas.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Run from root crontab
+# 0 0 * * 0 /usr/bin/flock -n /tmp/copy-nas.lockfile /home/htpc/projects/scripts/backup/copy-nas.sh
+#
+# Also I don't think flock likes sharing users, so for testing don't use flock OR use flock only with the user
+# you will using to run from cron.
+#
+# Read logger from /var/log/syslog, e.g. `tail /var/log/syslog`
+
+declare -a dirs=("/home/htpc/Music" "/home/htpc/Pictures" "/home/htpc/Videos" "/mnt/nas") 
+
+for DIR in "${dirs[@]}"
+do
+	localTarget="/home/htpc/nas/"
+        if [ -d "${DIR}" ]; then
+                logger "Copying $DIR to $localTarget"
+                rsync -rv "$DIR" "$localTarget"
+        else
+                logger "Could not find ${DIR}"
+        fi
+done
+
